ed363a229cc5aabcafd8f44776da0b3550f86660
realgenegreen <realgenegreen@gmail.com>
2023-09-11 20:09:46 +0300

    4

--- cypress/e2e/qctests.cy.js
+++ cypress/e2e/qctests.cy.js
@@ -68,68 +68,105 @@ context('by_hands', () => {
 
     }).then((response) => {
         expect(response.status, 'status').to.eq(200)
-      }) 
+        }) 
 
-    cy.get('.opblock-summary-delete > button:nth-child(1) > svg:nth-child(4)')
-      .click()
-    cy.get('.try-out__btn')
-      .click()
-    cy.get('table.parameters > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > input:nth-child(1)')
-      .click()
-      .type('5')
-    cy.get('.execute').click()
-    cy.reload()
+    let id
+
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:5001/users/whoami',
+
+    }).then(response => {
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.property('username', 'string')
+        expect(response.body).to.have.property('full_name', 'string')
+        expect(response.body).to.be.an('object')
+        expect(response.body.id).to.exist
+  
+      id = response.body.id
+        
+
+      cy.get('.opblock-summary-delete > button:nth-child(1) > svg:nth-child(4)')
+        .click()
+      cy.get('.try-out__btn')
+        .click()
+      cy.get('table.parameters > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > input:nth-child(1)')
+        .click()
+        .type(id.toString())
+      cy.get('.execute').click()
+      cy.reload()
+      })
   })
   
 })
 
+
+
 //requests
 
-  context('requests', () => {
-  it ('login+logout, whoami, and false delete jobs', () => {
+  context('by_requests', () => {
+  it ('login+logout, whoami and false delete jobs', () => {
 
     cy.visit('localhost:5001/docs#/') //cause don't like to see blank pages
     
     cy.request({
       method: 'POST',
+      url: 'http://localhost:5001/register',
+      body: {
+        'username': 'string', 
+        'full_name': 'string', 
+        'password': 'string'}
+
+    }).then((response) => {
+        expect(response.status, 'status').to.eq(200)
+        }) 
+
+    cy.request({
+      method: 'POST',
       url: 'http://localhost:5001/login',
       form: true,
-      
       body: 'grant_type=&username=string&password=string&scope=&client_id=&client_secret='
 
     }).then((response) => {
         expect(response.status, 'status').to.eq(200)
-      }) 
+        }) 
     
+    let id
+
     cy.request({
       method: 'GET',
       url: 'http://localhost:5001/users/whoami',
 
-    }).then((response) => {
-        expect(response.status, 'status').to.eq(200)
-        expect(response.body).to.have.property('username', 'string')
-        expect(response.body).to.have.property('full_name', 'string')
-      })  
-    
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:5001/logout',
+      }).then(response => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.have.property('username', 'string')
+          expect(response.body).to.have.property('full_name', 'string')
+          expect(response.body).to.be.an('object')
+          expect(response.body.id).to.exist
 
-    }).then((response) => {
-        expect(response.status, 'status').to.eq(200)
-        expect(response.body).to.have.property('message', 'logged out')
-      })
+        id = response.body.id
 
-    cy.request({
-      method: 'DELETE',
-      url: 'http://localhost:5001/user/5',
-      failOnStatusCode: false,
-   
-    }).then((response) => {
-        expect(response.status, 'status').to.eq(401)
-        expect(response.body).to.have.property('detail', 'Not authenticated')
-      })    
-    })
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:5001/logout',
+
+       }).then((response) => {
+            expect(response.status, 'status').to.eq(200)
+            expect(response.body).to.have.property('message', 'logged out')
+          })
+
+        cy.request({
+          method: 'DELETE',
+          url: 'http://localhost:5001/user/' + id,
+          failOnStatusCode: false
+
+         }).then((response) => {
+            expect(response.status, 'status').to.eq(401)
+            expect(response.body).to.have.property('detail', 'Not authenticated')
+          })  
+        })
+     
+  })
 
 //delete job
 
@@ -157,13 +194,12 @@ context('by_hands', () => {
 
       id = response.body.id
 
-    cy.request({
-      method: 'DELETE',
-      url: 'http://localhost:5001/user/' + id
-    })
+      cy.request({
+        method: 'DELETE',
+        url: 'http://localhost:5001/user/' + id
+        })
+      })
   })
-})
-
 
   })
 
